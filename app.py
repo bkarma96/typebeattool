4dc6935dfe6d4190ff7c156dab29aeb8f66cfdb8
bkarma96 <bkarma963@gmail.com>
2025-07-29 11:55:46 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+from googleapiclient.discovery import build
+import streamlit as st
+
+API_KEY = st.secrets["YOUTUBE_API_KEY"]
+youtube = build("youtube", "v3", developerKey=API_KEY)
+
+def search_videos(query):
+    req = youtube.search().list(
+        q=query,
+        part="snippet",
+        maxResults=10,
+        type="video"
+    )
+    res = req.execute()
+    videos = []
+    for item in res.get("items", []):
+        vids = {
+            "title": item["snippet"]["title"],
+            "channel": item["snippet"]["channelTitle"],
+            "videoId": item["id"]["videoId"]
+        }
+        videos.append(vids)
+    return videos
+
+st.title("TypeBeatTool 🎧")
+query = st.text_input("Enter Type Beat keyword (e.g., Drake Type Beat)")
+if st.button("Search"):
+    if query:
+        videos = search_videos(query + " type beat")
+        for vid in videos:
+            st.write(f"**{vid['title']}** by *{vid['channel']}*")
+            st.write(f"https://youtu.be/{vid['videoId']}")
